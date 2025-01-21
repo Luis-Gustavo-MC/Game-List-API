a73358f323008cf0ee7c20d99ff35800eefaf081
Luis Gustavo <lgmc120@gmail.com>
2025-01-21 12:14:38 -0300

    GameClass

---
+++ src/main/java/com/example/dslist/entities/Game.java
@@ -0,0 +1,61 @@
+package com.example.dslist.entities;
+
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tb_game")
+public class Game {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	@Column(name = "game_year")
+	private Integer year;
+	private String genre;
+	private String platforms;
+	private Double score;
+	private String imgUrl;
+	private String shortDescription;
+	private String LongDescription;
+	
+	public Game() {	
+	}
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
+			this.id = id;
+			this.title = title;
+			this.year = year;
+			this.genre = genre;
+			this.platforms = platforms;
+			this.score = score;
+			this.imgUrl = imgUrl;
+			this.shortDescription = shortDescription;
+			LongDescription = longDescription;
+	}
+	//Comparativo de dois objetos
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Game other = (Game) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
