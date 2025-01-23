277080440744121b54c108f69198414614dc4ad9
Luis Gustavo <lgmc120@gmail.com>
2025-01-23 14:46:58 -0300

    GameList , Belonging and BelongingPK

--- src/main/java/com/example/dslist/controllers/GameControler.java
+++ src/main/java/com/example/dslist/controllers/GameControler.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.dslist.DTO.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.repositories.GameRepository;
